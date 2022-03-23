8c24fdd1fb713876945b8c98b01f00c037b09b39
pingchunnn <alice92729@gmail.com>
2022-03-23 16:02:58 +0800

    加入專案檔案。

---
+++ 20220323-2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20220323_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] fib = new int[20];
+            fib[0] = 1;
+            fib[1] = 1;
+            for (int i = 2; i < fib.Length; i++)
+                fib[i] = fib[i-1] +　fib[i-2];
+            for (int i = 0; i < fib.Length; i++)
+                Console.WriteLine(fib[i]);
+            Console.ReadLine();
+        }
+    }
+}
